075103bc1f7c63d914ac73b4c9bb651315f52f35
Paulien van Alst <paulien@openvalue.nl>
2023-07-10 21:56:05 +0200

    Initial commit

---
+++ src/test/kotlin/com/openvalue/kotlin/coroutines/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.openvalue.kotlin.coroutines.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
